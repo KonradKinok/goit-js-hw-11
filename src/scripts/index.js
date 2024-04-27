b393000c9512d40855f3fe9bbb11490b645a5028
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-04-27 23:05:22 +0200

    27.04.2024

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+'use strict';
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const optionsNotify = {
+  timeout: 4000,
+};
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
