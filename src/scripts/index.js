d6a11c4f02067af2809e655ed53651ff0f6bffa2
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-04-28 23:17:46 +0200

    28.04.2024r.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,84 @@
 'use strict';
 import axios from 'axios';
+import * as pixabayMethods from './pixabay';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const optionsNotify = {
   timeout: 4000,
 };
-// Opisany w dokumentacji
+
 import SimpleLightbox from 'simplelightbox';
-// Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('form#search-form');
+const gallery = document.querySelector('div.gallery');
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  gallery.innerHTML = null;
+  const query = ev.currentTarget.elements.searchQuery.value;
+  console.log(query);
+  getPromisePictures(query);
+});
+
+function getPromisePictures(query) {
+  pixabayMethods
+    .fetchAllPictures(query)
+    .then(picturesColection => {
+      console.log(picturesColection);
+      renderPictures(picturesColection);
+    })
+    .catch(error => {
+      Notify.failure(`${error}`, optionsNotify);
+    });
+}
+
+function renderPictures(dataPictures) {
+  console.log(dataPictures.total, dataPictures.totalHits);
+  const markup = dataPictures.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        console.log(tags);
+        return `  <div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <div>
+              <p class="info-item">
+                <b>Likes</b>
+              </p>
+              <p class="info-item">${likes}</p>
+            </div>
+            <div>
+              <p class="info-item">
+                <b>Views</b>
+              </p>
+              <p class="info-item">${views}</p>
+            </div>
+            <div>
+              <p class="info-item">
+                <b>Comments</b>
+              </p>
+              <p class="info-item">${comments}</p>
+            </div>
+            <div>
+              <p class="info-item">
+                <b>Downloads</b>
+              </p>
+              <p class="info-item">${downloads}</p>
+            </div>
+          </div>
+        </div>`;
+      }
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
--- src/scripts/pixabay.js
+++ src/scripts/pixabay.js
@@ -1,3 +1,30 @@
 'use strict';
-
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const optionsNotify = {
+  timeout: 4000,
+};
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const apiKey = '43602379-82b2565bd0b0a0b53c6c265a8';
+
+/**fetchCatByBreed
+ *
+ * @param {string} breedId
+ * @returns object
+ */
+export async function fetchAllPictures(query) {
+  const searchParams = new URLSearchParams({
+    key: apiKey,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 40,
+    page: '1',
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+  console.log(url);
+  const response = await axios.get(url);
+  return response.data;
+}
